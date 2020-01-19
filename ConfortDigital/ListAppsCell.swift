05c64b32fd08bfb76c67e13fcdefc90cff106e4f
Aitor <aitor.ballesteros.pavon@gmail.com>
2020-01-19 12:54:17 -0800

    first commit

---
+++ ConfortDigital/ListAppsCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListAppsCell.swift
+//  ConfortDigital
+//
+//  Created by Aitor Ballesteros on 01/01/2020.
+//  Copyright © 2020 Aitor Ballesteros. All rights reserved.
+//
+
+import UIKit
+
+class ListAppsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ListAppsCell.swift
@@ -0,0 +1,9 @@
+//
+//  ListAppsCell.swift
+//  ConfortDigital
+//
+//  Created by Aitor Ballesteros on 01/01/2020.
+//  Copyright © 2020 Aitor Ballesteros. All rights reserved.
+//
+
+import Foundation
